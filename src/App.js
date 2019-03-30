6483529b5171201ac0c074629d992074857db2dd
kkuyu <devils67@naver.com>
2019-03-30 19:38:49 +0900

    component lifecycle test2

--- src/App.js
+++ src/App.js
@@ -2,45 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from "./Movie";
 
-const movies = [
-  {
-    title: "Muzi",
-    poster: "https://cdn.clien.net/web/api/file/F01/5250965/b5a2711ac555423094e.GIF"
-  },
-  {
-    title: "Apeach",
-    poster: "https://item.kakaocdn.net/do/f43d6e06d6563b7409e38f05f6df3666617ea012db208c18f6e83b1a90a7baa7"
-  },
-  {
-    title: "Prodo",
-    poster: "http://mblogthumb4.phinf.naver.net/20150421_291/dke0622_1429591776419oq62k_PNG/%C7%C1%B7%CE%B5%B52.png?type=w2"
-  },
-  {
-    title: "Con",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4kbMzSGzdvp3KXSM0_gi9YDPcoWkSNwFwWffaEkFlZNCfdEYPdQ"
-  }
-];
-
 class App extends Component {
   // Component Lifecycle [https://reactjs.org/docs/react-component.html#the-component-lifecycle]
   // Render: componentWillMount() => render() => componentDidMount()
   // Update : componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() => render() => componentDidMount()
   state = {
-    greeting: "Hellow!"
+    movies: [
+      {
+        title: "Muzi",
+        poster: "https://cdn.clien.net/web/api/file/F01/5250965/b5a2711ac555423094e.GIF"
+      },
+      {
+        title: "Apeach",
+        poster: "https://item.kakaocdn.net/do/f43d6e06d6563b7409e38f05f6df3666617ea012db208c18f6e83b1a90a7baa7"
+      },
+      {
+        title: "Prodo",
+        poster: "http://mblogthumb4.phinf.naver.net/20150421_291/dke0622_1429591776419oq62k_PNG/%C7%C1%B7%CE%B5%B52.png?type=w2"
+      },
+      {
+        title: "Con",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4kbMzSGzdvp3KXSM0_gi9YDPcoWkSNwFwWffaEkFlZNCfdEYPdQ"
+      }
+    ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting: "Hellow again!"
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Ryan",
+            poster: "https://i.pinimg.com/originals/94/4d/03/944d030fecf2a5d624dfdc5c9ef9f615.gif"
+          }
+        ]
       });
     }, 2000);
   }
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
